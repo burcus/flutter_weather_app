8b7bd165221738d47f3b6d7c98d128cb4adff95e
Burcu Söylemez <burcu.soylemez@bil.omu.edu.tr>
2020-09-12 11:05:01 +0300

    use blocConsumer instead of builder

--- lib/main.dart
+++ lib/main.dart
@@ -42,20 +42,30 @@ class HomePage extends StatelessWidget {
     final _weatherBloc = BlocProvider.of<WeatherBloc>(context);
 
     return Scaffold(
-      body: BlocBuilder(
+      body: BlocConsumer(
+          listener: (BuildContext context, state) {
+            if(state is LocationInitialState) _locationBloc.add(CheckPermission());
+            if(state is Always) _locationBloc.add(GetLocation());
+            if(state is LocationSucceed) _weatherBloc.add(GetWeatherInfo(state.city));
+          },
+          builder: (BuildContext context, state){
+            if(state is LocationFailed) return SearchScreen();
+            if(state is LoadedState) return PresentScreen(state.weather);
+            else return CircularProgressIndicator();
+          }
+      ),
+      //todo use bloc consumer
+/*      body: BlocBuilder(
         cubit: _locationBloc,
         builder: (context, state) {
           if(state is LocationInitialState) _locationBloc.add(CheckPermission());
           if(state is Always) _locationBloc.add(GetLocation());
           if(state is LocationFailed) return SearchScreen();
           if(state is LocationSucceed) _weatherBloc.add(GetWeatherInfo(state.city));
-          if(state is LoadedState) {
-            print("loadded stateeeeeee");
-            return PresentScreen(state.weather);
-          };
+          if(state is LoadedState) return PresentScreen(state.weather);
           return CircularProgressIndicator();
         },
-      ),
+      ),*/
     );
   }
 }
