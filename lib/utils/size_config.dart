be846eef59e2b51cf732f9d973e598c8cba5db7e
Burcu Söylemez <burcu.soylemez@bil.omu.edu.tr>
2020-10-17 12:10:13 +0300

    create size configuration file for make responsive ui

---
+++ lib/utils/size_config.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+
+class SizeConfig {
+  static double _screenWidth;
+  static double _screenHeight;
+  static double _safeAreaHeight;
+  static var _safeAreaPadding;
+  static double _safeAreaVertical;
+
+  static double _blockWidth;
+  static double _blockHeight;
+
+  static double textMultiplier;
+  static double imageSizeMultiplier;
+  static double heightMultiplier;
+  static double widthMultiplier;
+
+  void init(BuildContext context, Orientation orientation) {
+    _safeAreaPadding = MediaQuery.of(context).padding;
+    _safeAreaVertical = _safeAreaPadding.top + _safeAreaPadding.bottom;
+
+    _screenHeight = MediaQuery.of(context).size.height;
+    _screenWidth = MediaQuery.of(context).size.width;
+
+    _safeAreaHeight = _screenHeight - _safeAreaVertical;
+    if(orientation == Orientation.landscape) _safeAreaHeight = _screenHeight;
+
+    _blockWidth = _screenWidth / 100;
+    _blockHeight = _safeAreaHeight / 100;
+
+    textMultiplier = _blockHeight;
+    imageSizeMultiplier = _blockWidth;
+    heightMultiplier = _blockHeight;
+    widthMultiplier = _blockWidth;
+  }
+}
