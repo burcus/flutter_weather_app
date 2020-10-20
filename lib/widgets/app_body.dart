78199d74330c5bb7e88904de94f3448a48eaf508
Burcu Söylemez <burcu.soylemez@bil.omu.edu.tr>
2020-10-20 06:30:54 +0300

    remove shader mask

--- lib/widgets/app_body.dart
+++ lib/widgets/app_body.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutterweatherapp/blocs/blocs.dart';
 import 'package:flutterweatherapp/utils/utils.dart';
 import 'package:flutterweatherapp/widgets/widgets.dart';
 
 class AppBody extends StatelessWidget {
-  final String imagePath;
-
-  AppBody(this.imagePath);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,19 +17,7 @@ class AppBody extends StatelessWidget {
               Stack(
                 alignment: Alignment.center,
                 children: <Widget>[
-                  ShaderMask(
-                      shaderCallback: (rect) {
-                        return LinearGradient(
-                                colors: [
-                              CustomColors.softGray.withOpacity(0.9),
-                              CustomColors.softGray.withOpacity(0.8),
-                              //Colors.white.withOpacity(0.5),
-                            ],
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter)
-                            .createShader(rect);
-                      },
-                      child: weatherThemeImage(context, imagePath)),
+                  WeatherThemeImage(),
                   ListingScreenSearchBar(),
                   ListingScreenBody(),
                   ListingScreenCards(),
